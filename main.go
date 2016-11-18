c904e4826772a3b756fbf618e145067ab5fa2923
Abdul Dakkak <abduld@wolfram.com>
2016-11-18 05:46:10 -0700

    update for bluemix deployment ... some fields need to be variables in the pipeline

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "bitbucket.org/c3sr/p3sr-demo-webserver-bluemix/web"
+import (
+	"log"
+	"os"
+
+	"bitbucket.org/c3sr/p3sr-demo-webserver-bluemix/web"
+)
+
+const (
+	DEFAULT_PORT = "8080"
+)
 
 func main() {
+
+	var port string
+	if port = os.Getenv("PORT"); len(port) == 0 {
+		port = DEFAULT_PORT
+	}
+
+	log.Printf("Starting app on port %+v\n", port)
 	web.Start(":1323")
 }
